3dc62e76442001103670544cbca2cb2d8a083332
Ubuntu <ubuntu@ip-172-31-32-146.us-west-2.compute.internal>
2016-11-06 23:38:07 +0000

    testing

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -6,7 +6,7 @@ class IncidentsController < ApplicationController
   def index
     add_breadcrumb "Incidents", :incidents_path
     @incidents = Incident.paginate(:page => params[:page], :per_page => 10)
-    TextBelt.text('8062907304', 'Hello, world!')
+    TextBelt.text("18062907304", 'Hello, world!')
   end
 
   def show
