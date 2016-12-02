de1ffa62739d48c07f812bb006cbdd0708ef1cf8
Cody A. Galbreath <galbreathcody@gmail.com>
2016-12-01 22:36:03 -0600

    Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_filter :set_global_search_variable
-  auto_session_timeout 30.minutes
+  auto_session_timeout 15.minutes
 
   def set_global_search_variable
     @q = Incident.search(params[:q])
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password)}
   end
 end
