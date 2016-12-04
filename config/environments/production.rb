491467cb19862ee005c7881619e19d7381797369
Ubuntu <ubuntu@ip-172-31-32-146.us-west-2.compute.internal>
2016-12-04 03:12:18 +0000

    fix for new beanstalk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = { :host => 'bgc-incident-reporting-env.zvaixqq5vp.us-west-2.elasticbeanstalk.com' }
+  config.action_mailer.default_url_options = { :host => 'http://rails-incident-reporting-env.qqma4wpuik.us-west-2.elasticbeanstalk.com' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
