c88ddb283c75a0cb271167c550eb3f7923736e3d
Ubuntu <ubuntu@ip-172-31-32-146.us-west-2.compute.internal>
2016-12-05 03:25:21 +0000

    updated wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 WickedPdf.config = {
-  :wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  # :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => '/usr/local/bin/wkhtmltopdf'
   #exe_path: "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"
 }
